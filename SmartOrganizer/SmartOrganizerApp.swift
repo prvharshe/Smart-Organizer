83de6817a00f7662bb2a56a5159f6fec9ceb2dbb
Pranav Harshe <pranav.harshe94@gmail.com>
2025-03-05 20:06:00 +0530

    Initial Commit

---
+++ SmartOrganizer/SmartOrganizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmartOrganizerApp.swift
+//  SmartOrganizer
+//
+//  Created by Pranav Harshe on 05/03/25.
+//
+
+import SwiftUI
+
+@main
+struct SmartOrganizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
